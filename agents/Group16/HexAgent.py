89909836de60d7e2bef1e07f3d2ac40ec3121d30
Sasha Armstrong <hefalumpkin38@gmail.com>
2025-11-21 11:33:16 +0000

    Initial commit

---
+++ agents/Group16/HexAgent.py
@@ -0,0 +1,10 @@
+from src.AgentBase import AgentBase
+
+class HexAgent(AgentBase):
+    def __init__(self, player):
+        super().__init__(player)
+        # Initialize any additional attributes here
+
+    def get_move(self, game_state):
+        # Implement the logic to choose a move based on the current game state
+        pass
